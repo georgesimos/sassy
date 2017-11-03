80997778a937ea55e483ae75e3b070b4fe05487f
George Simos <georgesimos@hotmail.gr>
2017-11-03 21:25:48 +0200

    initial Commit
    index.html

--- gruntfile.js
+++ gruntfile.js
@@ -1,74 +1,75 @@
 // The "wrapper" function
 module.exports = function(grunt) {
-
-  // Project and task configuration
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+  
+    // Project and task configuration
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      /*
+        Grunt Sass
+        Compile Sass to CSS using node-sass
+        https://www.npmjs.com/package/grunt-sass
+      */
+      sass: {
+  
+      options: {
+          sourceMap: false
+      },
+      dist: {
+          files: {
+              'css/styles.css': 'assets/scss/styles.scss'
+          }
+      }
+    },
+  
     /*
-      Grunt Sass
-      Compile Sass to CSS using node-sass
-      https://www.npmjs.com/package/grunt-sass
+      Grunt Contrib Watch
+      Monitor files and excute tasks
+      https://www.npmjs.com/package/grunt-contrib-watch
     */
-    sass: {
-
-    options: {
-        sourceMap: false
+    watch:{
+  
+      sass: {
+  
+        files: [
+          'assets/scss/**/*.scss'
+        ],
+        tasks: [
+          'sass'
+        ]
+      },
+      scripts: {
+  
+        files: [
+          'assets/js/*.js'
+        ],
+        tasks: [
+          'uglify'
+        ]
+      }
     },
-    dist: {
+    /*
+      Grunt Contrib Uglify
+      Minify JS files
+      https://www.npmjs.com/package/grunt-contrib-uglify
+    */
+    uglify: {
+  
+      my_target: {
+  
         files: {
-            'css/styles.css': 'assets/scss/styles.scss'
+          'js/scripts.js' : ['assets/js/scripts.js']
         }
-    }
-  },
-
-  /*
-    Grunt Contrib Watch
-    Monitor files and excute tasks
-    https://www.npmjs.com/package/grunt-contrib-watch
-  */
-  watch:{
-
-    sass: {
-
-      files: [
-        'assets/scss/*.scss'
-      ],
-      tasks: [
-        'sass'
-      ]
-    },
-    scripts: {
-
-      files: [
-        'assets/js/*.js'
-      ],
-      tasks: [
-        'uglify'
-      ]
-    }
-  },
-  /*
-    Grunt Contrib Uglify
-    Minify JS files
-    https://www.npmjs.com/package/grunt-contrib-uglify
-  */
-  uglify: {
-
-    my_target: {
-
-      files: {
-        'js/scripts.js' : ['assets/js/scripts.js', 'node_modules/jquery/dist/jquery.js']
       }
     }
-  }
-
-
-
-  });
-
-  // Loading Grunt plugins and tasks
-  require('load-grunt-tasks')(grunt);
-
-  // Custom tasks
-  grunt.registerTask('default', ['watch']);
-};
+  
+  
+  
+    });
+  
+    // Loading Grunt plugins and tasks
+    require('load-grunt-tasks')(grunt);
+  
+    // Custom tasks
+    grunt.registerTask('default', ['watch']);
+  };
+  
